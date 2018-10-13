8f1e993309cdc9c87542f5ebcf8828aba3d4d3ed
gbear605 <developer@vivissi.com>
2018-10-13 02:47:59 -0400

    Remove unneeded imports in backend tests

--- backend/src/test/java/commusic/OutputControllerTest.java
+++ backend/src/test/java/commusic/OutputControllerTest.java
@@ -1,6 +1,5 @@
 package commusic;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -10,12 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import commusic.output.OutputController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
